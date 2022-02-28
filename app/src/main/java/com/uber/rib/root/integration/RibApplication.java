9c7515a1527d681e3e36fc0d016b3748ccb73e8e
Tony Tang <tonytang@uber.com>
2022-02-27 21:17:50 -0800

    Add Timber log.

--- app/src/main/java/com/uber/rib/root/integration/RibApplication.java
+++ app/src/main/java/com/uber/rib/root/integration/RibApplication.java
@@ -1,18 +1,29 @@
 package com.uber.rib.root.integration;
 
 import android.app.Application;
+import timber.log.ThreadTree;
+import timber.log.Timber;
 
 public class RibApplication extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
+    initLog();
+  }
+
+  private void initLog() {
+    Timber.plant(new ThreadTree("Tony_Gym"));
     System.out.println("onCreate");
   }
 
   @Override
   public void onTerminate() {
     super.onTerminate();
+    onTerminated();
+  }
+
+  private void onTerminated() {
     System.out.println("onTerminate");
   }
 }
